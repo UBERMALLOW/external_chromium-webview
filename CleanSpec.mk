08f42a5159fda0e96524c7596bffc42b1c2d4202
Chet Kener <Cl3Kener@gmail.com>
2015-10-29 19:53:37 -0400

    Fix CleanSpec Locations
    * No libs are needed. We insert apk that contains libs. Also rename location to WebViewGoogle
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -43,9 +43,9 @@
 #$(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/core_intermediates)
 #$(call add-clean-step, find $(OUT_DIR) -type f -name "IGTalkSession*" -print0 | xargs -0 rm -f)
 #$(call add-clean-step, rm -rf $(PRODUCT_OUT)/data/*)
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib/libwebviewchromium.so)
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib64/libwebviewchromium.so)
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/webview)
+#$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib/libwebviewchromium.so)
+#$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib64/libwebviewchromium.so)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/WebViewGoogle)
 
 # ******************************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST ABOVE THIS BANNER
